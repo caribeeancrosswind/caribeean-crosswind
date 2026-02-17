e30fdcb1165493fe7c9fafd32430ac9b71f53242
caribeeancrosswind <rincondominicanocl@gmail.com>
2026-02-17 01:36:34 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,5 @@
-// REEMPLAZA CON TUS DATOS DE SUPABASE
 const SUPABASE_URL = "https://tvzgbzmgoklweddcxqbb.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_4KUtTNvezxKzgILwkHwYew_LJrKh93n";
+const SUPABASE_ANON_KEY = "sb_publishable_4KUtTNvexKzglLwkHwYew_LJrKh93n";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
